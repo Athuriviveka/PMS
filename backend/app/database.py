b668729456b396274fe7d4559d661e6c61c2c3da
Athuri Vivekanantharasa <vivekaathuri@gmail.com>
2025-01-11 13:30:17 -0500

    db chnages

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://vivekaathuri:p3JxsIwYYtE6e0Ay@pms.xq5f6.mongodb.net/?retryWrites=true&w=majority&appName=PMS")
+client = MongoClient("mongodb+srv://user:pw@pms.xq5f6.mongodb.net/?retryWrites=true&w=majority&appName=PMS")
 db = client["payment_db"]  # Database name
 payments_collection = db["payments"]  # Collection name
 
